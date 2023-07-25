8693299bc4b62552fe52a854a84deabe7bdbde0a
Gilbert Haro <gilbertharocode@gmail.com>
2023-07-25 12:52:40 -0700

    testimonial text

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,17 @@ function App() {
     <>
       <div className='carousel'>
         <img src={img} alt='Maria de Almeida' />
+        <blockqoute className='testimonial'>
+          <p className='testimonial-text'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
+            nesciunt aliquid ex atque quibusdam. Rerum officia unde suscipit quo
+            sunt hic illo fugit.
+          </p>
+          <p className='testimonial-author'>Maria de Almeida</p>
+          <div className='testimonial-job'>
+            Senior Product Manager at EDP Comercial
+          </div>
+        </blockqoute>
       </div>
     </>
   );
